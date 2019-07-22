37447143251966d2f252ab3a245bd7f8b5e57491
Josh <cowley.josh@gmail.com>
2019-07-22 19:06:25 -0400

    Completed tabs.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,4 +8,47 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+let url = 'https://lambda-times-backend.herokuapp.com/topics';
 
+ // Data Function
+function topicData (url) {
+
+     // Get topics from url
+    axios.get(url)
+    .then( response => {
+
+           // Get topics  
+          let topicContent = []; 
+          topicContent = response["data"]["topics"];
+
+           // Create Topics
+          for(i in topicContent){
+
+                 CreateTabs(topicContent[i]);
+
+           };                 
+
+     })
+     .catch( error => {
+         console.log("Error:", error); // If there is an error, log it to the console.
+     })
+
+ };
+
+ // Created Tabs
+function CreateTabs(tabTopic){
+
+     // Get the main tab DIV
+    let mainTabDiv = document.getElementsByClassName("topics")[0];
+
+     // Create a new DIV
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('tab'); 
+    newDiv.innerText = tabTopic;
+
+     // Add the new DIV 
+    mainTabDiv.appendChild(newDiv);
+
+  };
+
+topicData(url);
